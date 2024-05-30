b74def254bb772534cc4d35bf4a1e68664f08c3c
Euan Turner <euanoturner@gmail.com>
2024-05-31 00:36:42 +0100

    Remove duplicate table

--- src/app/options/option/page.tsx
+++ src/app/options/option/page.tsx
@@ -33,20 +33,6 @@ export default async function Page() {
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
       </div>
-      <div className="w-1/2 mx-auto grid p-4">
-        <div className="flex items-center bg-white py-4 border-y-2">
-          <div className="flex-1 text-lg font-bold">Date</div>
-          <div className="text-lg font-bold">Title</div>
-        </div>
-        {evs.map((ev) => (
-          <div key={ev.id} className="flex items-center bg-white py-4 border-b-2">
-            <div className="flex-1 text-sm text-gray-500 font-bold">
-              {ev.date.getDate()}/{ev.date.getMonth() + 1}
-            </div>
-            <div className="text-lg">{ev.title}</div>
-          </div>
-        ))}
-      </div>
       <Link className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none" href="/options/add-evidence">Add Evidence</Link>
     </div>
   );
