90f17c4988c84190fda4b50b856e3a9fb021499e
James Watling <james.watling@gmail.com>
2024-05-31 12:05:40 +0100

    Sample change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <p>
-          Dashboard
+          Hello
         </p>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
