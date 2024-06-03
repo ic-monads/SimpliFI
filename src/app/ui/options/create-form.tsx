5f95265abaea27fbe777a7b5e9533ee1f3d21745
James Watling <james.watling@gmail.com>
2024-06-03 16:35:43 +0100

    refactor(M3-4): :lipstick: Use DaisyUI in create-form, slightly restyle sidenav

--- src/app/ui/options/create-form.tsx
+++ src/app/ui/options/create-form.tsx
@@ -6,24 +6,27 @@ import { createOption } from '@/app/lib/actions';
 export default function Form({ actions, parcels }: { actions: Action[], parcels: LandParcel[] }) {
   return (
     <form action={createOption}>
+      <h1 className="font-semibold text-3xl">Add Option</h1>
       <div className="max-w-md">
-        <div className="mb-2 block">
-          <Label htmlFor="actions" value="Select SFI Action" />
+        <div className="label">
+          <label htmlFor="actions" className="label-text">Select SFI Action</label>
         </div>
-        <Select id="actions" name="actionCode" required>
+        <select id="actions" name="actionCode" className="select select-bordered max-w-md" required>
+          <option disabled selected>Choose Action</option>
           {actions.map((a) => <option key={a.code}>{a.name}</option>)}
-        </Select>
-        <div className="mb-2 block">
-          <Label htmlFor="parcels" value="Select Land Parcel" />
+        </select>
+        <div className="label">
+          <label htmlFor="parcels" className="label-text">Select Land Parcel</label>
         </div>
-        <Select id="parcels" name="parcelId" required>
-          {parcels.map((p) => <option key={p.id}>{`${p.name}(${p.id})`}</option>)}
-        </Select>
+        <select id="actions" name="actionCode" className="select select-bordered" required>
+          <option disabled selected>Choose Parcel</option>
+          {parcels.map((p) => <option key={p.id}>{`${p.name} (${p.id})`}</option>)}
+        </select>
         <div className="mt-6 flex justify-end gap-4">
           <Link href="/options">
-            <Button color={"light"}>Cancel</Button>
+            <button className="btn btn-neutral-content">Cancel</button>
           </Link>
-          <Button color={"success"} type="submit">Create Option</Button>
+          <button className="btn btn-primary" type="submit">Create Option</button>
         </div>
       </div>
     </form>
