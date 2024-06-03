4efa3a599e9c035f54e1f72c249cf006368d2420
Euan Turner <euanoturner@gmail.com>
2024-06-03 23:02:49 +0100

    feat: :sparkles: Add notes field for evidence

--- src/app/ui/options/option/create-evidence-form.tsx
+++ src/app/ui/options/option/create-evidence-form.tsx
@@ -38,39 +38,43 @@ export default function Form({ actCode, parcelId }: { actCode: string, parcelId:
 
   return (
     <div className="mx-auto">
-      <h1 className="font-semibold text-xl mb-2">Add Evidence</h1>
-      { error && <p className="text-red-500 text-sm mb-5">{error}</p> }
-      <form className="max-w-sm form-control" action={handleSubmit}>
-        <div className="label">
-          <label htmlFor="title" className="label-text">Title</label>
+            <h1 className="font-semibold text-xl mb-2">Add Evidence</h1>
+            { error && <p className="text-red-500 text-sm mb-5">{error}</p> }
+            <form className="max-w-sm" action={handleSubmit}>
+              <div className="mb-5">
+                <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                <input type="text" id="title" name="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+              </div>
+              <div className="mb-5">
+                  <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900">Date</label>
+                  <input type="date" id="date" name="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5" required/>
+              </div>
+              <div className="mb-5">
+                  <label htmlFor="notes" className="block mb-2 text-sm font-medium text-gray-900">Notes</label>
+                  <input type="notes" id="notes" name="notes" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5" required/>
+              </div>
+              <div className="mb-5">
+                  <label htmlFor="file" className="block mb-2 text-sm font-medium text-gray-900">File</label>
+                  <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" type="file" ref={fileInputRef} id="file" name="file" required />
+              </div>
+              <div className="mb-5">
+                  <label htmlFor="actCode" className="block mb-2 text-sm font-medium text-gray-900">Action Code</label>
+                  <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="actCode" name="actCode" type="text" value={actCode} readOnly/>
+              </div>
+              <div className="mb-5">
+                  <label htmlFor="parcelId" className="block mb-2 text-sm font-medium text-gray-900">Parcel Id</label>
+                  <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="parcelId" name="parcelId"type="text" value={parcelId} readOnly/>
+              </div>
+              <div className="mt-6 flex justify-end gap-4">
+                <Link href={{
+                  pathname: "/options/option",
+                  query: { actCode, parcelId }
+                }}>
+                  <button className="btn btn-content-neutral">Cancel</button>
+                </Link>
+                <button className="btn btn-primary"type="submit">Submit Evidence</button>
+              </div>
+            </form>
         </div>
-        <input type="text" id="title" name="title" className="input input-bordered" required />
-        <div className="label">
-          <label htmlFor="date" className="label-text">Date</label>
-        </div>
-        <input type="date" id="date" name="date" className="input input-bordered" required/>
-        <div className="label">
-          <label htmlFor="file" className="label-text">File</label>
-        </div>
-        <input className="file-input file-input-bordered" type="file" ref={fileInputRef} id="file" name="file" required />
-        <div className="label">
-          <label htmlFor="actCode" className="label-text">Action Code</label>
-        </div>
-        <input className="input input-bordered" id="actCode" name="actCode" type="text" value={actCode} readOnly/>
-        <div className="label">
-          <label htmlFor="parcelId" className="label-text">Parcel Id</label>
-        </div>
-        <input className="input input-bordered" id="parcelId" name="parcelId" type="text" value={parcelId} readOnly/>
-        <div className="mt-6 flex justify-end gap-4">
-          <Link href={{
-            pathname: "/options/option",
-            query: { actCode, parcelId }
-          }}>
-            <button className="btn btn-neutral-content">Cancel</button>
-          </Link>
-          <Submit text="Submit Evidence" />
-        </div>
-      </form>
-    </div>
   )
 }
