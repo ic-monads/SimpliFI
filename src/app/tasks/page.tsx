a95864fe38a08be8a4faa99cba12d4968c3817a7
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-04 19:13:48 +0100

    feat(Tasks): :lipstick: Update style for consistency with task page

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -7,15 +7,15 @@ export default async function Page() {
   const tasks = await fetchAllTasks();
   return (
     <div className="w-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-semibold">My Tasks</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">All Tasks</h1>
         <Link href="/tasks/add">
           <button className="btn btn-primary">Add Task</button>
         </Link>
       </div>
       <div className="pt-2 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {tasks.map((t) => (
-          <Card key={t.id} title={t.title} id={t.id} actCode={t.actCode} parcelId={t.parcelId} />
+        {tasks.map((task) => (
+          <Card key={task.id} task={task} />
         ))}
       </div>
     </div>
