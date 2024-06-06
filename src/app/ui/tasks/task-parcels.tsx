a4a77ccaa8c16ea4deea777b57b1039947887822
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-06 19:11:51 +0100

    fix: :bug: Add key to task parcels

--- src/app/ui/tasks/task-parcels.tsx
+++ src/app/ui/tasks/task-parcels.tsx
@@ -4,7 +4,7 @@ export default async function TaskParcels({ taskId }: { taskId: string }) {
   const parcels = await fetchTaskParcels(taskId);
   return (
     <div className="flex flex-wrap">
-      { parcels.map((parcel) => <div className="badge badge-outline text-xs mr-2 mt-2 text-nowrap">{parcel.name} ({parcel.id})</div>) }
+      { parcels.map((parcel) => <div key={parcel.id} className="badge badge-outline text-xs mr-2 mt-2 text-nowrap">{parcel.name} ({parcel.id})</div>) }
     </div>
   )
 }
