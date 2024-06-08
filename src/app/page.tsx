4d4a65ac066ad4910f24a4c6ab5ace4bca43de14
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-08 22:43:16 +0100

    fix(Routing): :bug: Redirect from root path to actions page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import Image from "next/image";
-import Link from 'next/link';
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <p>
-          Dashboard
-        </p>
-      </div>
-    </main>
-  );
+  redirect('/actions');
 }
