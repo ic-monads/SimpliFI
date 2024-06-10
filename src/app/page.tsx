edda7a03192e8d67775b5a3749a7e423a0310250
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-10 15:49:58 +0100

    feat: :sparkles: Add form to root path with SBI input so that users can be redirected to the correct farm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
 import { redirect } from "next/navigation";
+import Submit from "./components/Submit";
 
 export default function Home() {
-  redirect('/actions');
+  const handleSubmit = async (formData: FormData) => {
+    redirect(`${formData.get('sbi')}/actions`);
+  }
+
+  return(
+    <div className="w-full flex justify-center">
+      <div className="mx-auto mt-10">
+        <form className="max-w-sm" action={handleSubmit}>
+          <div className="label">
+            <label htmlFor="sbi" className="label-text">SBI</label>
+          </div>
+          <input type="text" id="sbi" name="sbi" className="input input-bordered w-full" required />
+          <div className="mt-6 flex justify-center gap-4">
+            <Submit text="Manage my farm" />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
