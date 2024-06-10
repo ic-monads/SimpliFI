feed43dd21b4cff33968b2e655fff44d1fcf1379
Euan Turner <euanoturner@gmail.com>
2024-06-10 12:40:53 +0100

    stuff

--- src/app/actions/[actionCode]/ParcelMap.tsx
+++ src/app/actions/[actionCode]/ParcelMap.tsx
@@ -2,21 +2,33 @@
 
 import React from 'react';
 import {useState, useEffect } from 'react';
-import Map, { Source, Layer, GeolocateControl, NavigationControl,  GeoJSONSource} from 'react-map-gl';
-import { FeatureCollection } from 'geojson';
+import Map, { Source, Layer, GeolocateControl, NavigationControl } from 'react-map-gl';
+import { FeatureCollection, Feature } from 'geojson';
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN; // Set your mapbox token here
+
+function mapFeature(feat: Feature): Feature {
+  return {
+    type: feat.type,
+    geometry: feat.geometry,
+    properties: feat.properties
+  };
+}
 
 export default function ParcelMap({ sbi }: { sbi: string }) {
   const [data, setData] = useState<FeatureCollection | undefined>(undefined);
   useEffect(() => {
     /* global fetch */
-    fetch(
+    const fc = fetch(
       `/api/map?sbi=${sbi}`
     )
       .then((resp) => resp.json())
-      .then((geojson: FeatureCollection) => {setData(geojson); console.log(geojson)})
+      .then((geojson) => ({
+        type: 'FeatureCollection',
+        features: geojson.features.map(mapFeature) as Feature[]
+      }))
+      .then((geojson) => {setData(geojson as FeatureCollection); console.log(geojson)})
       .catch(err => console.error('Could not load data', err)); // eslint-disable-line
     }, [sbi]);
   console.log(data);
