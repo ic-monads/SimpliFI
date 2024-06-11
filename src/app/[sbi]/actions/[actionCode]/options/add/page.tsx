e4ec6ae215c55b3a3b194c31a25671063d964cb5
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-11 12:23:35 +0100

    fix(Options): :bug: Pass sbi to createOptionForAction

--- src/app/[sbi]/actions/[actionCode]/options/add/page.tsx
+++ src/app/[sbi]/actions/[actionCode]/options/add/page.tsx
@@ -6,11 +6,11 @@ import CancelButton from "@/app/components/CancelButton";
 export default async function Page({ params }: { params: { sbi: string, actionCode: string } }) {
   /* TODO: Change to be actions that a farmer is performing */
   const { sbi, actionCode } = params;
-  const parcels = await fetchFarmLandParcels();
+  const parcels = await fetchFarmLandParcels(sbi);
   return (
     <div className="flex flex-col items-center">
       <h1 className="font-semibold text-2xl mb-3">Add Option</h1>
-      <form className="max-w-md" action={createOptionForAction.bind(null, params.actionCode)}>
+      <form className="max-w-md" action={createOptionForAction.bind(null, sbi, actionCode)}>
         <div className="label">
           <label htmlFor="parcels" className="label-text">Select Land Parcel</label>
         </div>
