99eb2269346102768d1a5d29358dfa44d999fd4e
Euan Turner <euanoturner@gmail.com>
2024-06-11 21:36:00 +0100

    feat: :lipstick: Truncate badges

--- src/app/components/ActionBadges.tsx
+++ src/app/components/ActionBadges.tsx
@@ -2,8 +2,12 @@ import { Action } from "@prisma/client";
 
 export default function ActionBadges({ actions } : { actions: Action[] }) {
   return (
-    <div className="flex flex-wrap">
-      { actions.map((action) => <div key={action.code} className="badge badge-ghost text-xs mr-2 mt-2 text-nowrap">{action.name} ({action.code})</div>) }
+    <div className="flex overflow-hidden gap-y-2">
+      {actions.map((action) => 
+        <div key={action.code} className="badge badge-ghost text-xs mr-2 mt-2 max-w-[33%] overflow-hidden text-ellipsis whitespace-nowrap">
+          <span className="inline-block align-top truncate">{action.name} ({action.code})</span>
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- src/app/components/ParcelBadges.tsx
+++ src/app/components/ParcelBadges.tsx
@@ -2,8 +2,14 @@ import { LandParcel } from "@prisma/client";
 
 export function ParcelBadges({ parcels } : { parcels: LandParcel[] }) {
   return (
-    <div className="flex flex-wrap gap-y-2">
-      { parcels.map((parcel) => <div key={parcel.id} className="badge badge-ghost text-xs mr-2 text-nowrap">{parcel.name} ({parcel.id})</div>) }
+    <div className="flex overflow-hidden gap-y-2">
+      {parcels.map((parcel) => 
+      <div key={parcel.id} className="badge badge-ghost text-xs mr-2 mt-2 max-w-[33%] overflow-hidden text-ellipsis whitespace-nowrap">
+          <span className="inline-block align-top truncate">
+            {parcel.name} ({parcel.id})
+          </span>
+        </div>
+      )}
     </div>
   )
 }
