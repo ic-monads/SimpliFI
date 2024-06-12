529f1eaaf8a61de33bf9a81f7a09d9f973f1ffb7
Euan Turner <euanoturner@gmail.com>
2024-06-12 22:45:26 +0100

    feat: :sparkles: Display important farm dates in SideNav

--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -1,6 +1,9 @@
 import NavLinks from '@/app/components/NavLinks';
+import { fetchFarm } from '@/app/server-actions/farm';
+import Moment from 'moment';
 
-export default function SideNav({ sbi }: { sbi: string }) {
+export default async function SideNav({ sbi }: { sbi: string }) {
+  const farm = await fetchFarm(sbi);
   return (
     <div className="flex items-center md:items-stretch md:flex-col border-b h-full px-3 py-4 md:border-r shadow-sm">
       <h1 className='text-center font-semibold mr-10 md:mr-0 md:py-10 text-2xl'><span className="text-green-600">S</span>impli<span className="text-green-600">FI</span></h1>
@@ -11,12 +14,24 @@ export default function SideNav({ sbi }: { sbi: string }) {
 
       <div className="flex flex-row space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <h2 className='text-left font-semibold mr-10 md:mr-0 pt-4 pb-2 text-xl'>Your Dates</h2>
-        <h3 className="text-lg">Agreement Start</h3>
-        <p className="text-sm">Placeholder</p>
-        <h3 className="text-lg">Agreement End</h3>
-        <p className="text-sm">Placeholder</p>
-        <h3 className="text-lg">Agreement Renewal</h3>
-        <p className="text-sm">Placeholder</p>
+        { farm.startAg && (
+          <>
+            <h3 className="text-lg">Agreement Start</h3>
+            <p className="text-sm">{Moment(farm.startAg).format('DD/MM/YYYY')}</p>
+            </>
+          )}
+        { farm.endAg && (
+          <>
+            <h3 className="text-lg">Agreement Start</h3>
+            <p className="text-sm">{Moment(farm.endAg).format('DD/MM/YYYY')}</p>
+            </>
+          )}
+        { farm.renewAg && (
+        <>
+          <h3 className="text-lg">Agreement Start</h3>
+          <p className="text-sm">{Moment(farm.renewAg).format('DD/MM/YYYY')}</p>
+          </>
+        )}
       </div>
     </div>
   );
