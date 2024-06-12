1e31ab367e2a48c5abe763749ed5c7fb8a9e4708
Euan Turner <euanoturner@gmail.com>
2024-06-12 20:39:55 +0100

    feat(Dates): :sparkles: DAdd dates to side nav

--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -4,8 +4,19 @@ export default function SideNav({ sbi }: { sbi: string }) {
   return (
     <div className="flex items-center md:items-stretch md:flex-col border-b h-full px-3 py-4 md:border-r shadow-sm">
       <h1 className='text-center font-semibold mr-10 md:mr-0 md:py-10 text-2xl'><span className="text-green-600">S</span>impli<span className="text-green-600">FI</span></h1>
+
       <div className="flex flex-row space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks sbi={sbi} />
+      </div>
+
+      <div className="flex flex-row space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <h2 className='text-left font-semibold mr-10 md:mr-0 pt-4 pb-2 text-xl'>Your Dates</h2>
+        <h3 className="text-lg">Agreement Start</h3>
+        <p className="text-sm">Placeholder</p>
+        <h3 className="text-lg">Agreement End</h3>
+        <p className="text-sm">Placeholder</p>
+        <h3 className="text-lg">Agreement Renewal</h3>
+        <p className="text-sm">Placeholder</p>
       </div>
     </div>
   );
