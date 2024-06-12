dcd5109f27906f00045a3ab3b86143b6c9e3355e
Euan Turner <euanoturner@gmail.com>
2024-06-12 15:18:25 +0100

    feat(Parcels): :sparkles: Switch to react-map-gl maps

--- src/app/components/ParcelMap.tsx
+++ src/app/components/ParcelMap.tsx
@@ -1,66 +1,45 @@
 "use client";
 
-import React from "react";
-import mapboxgl from "mapbox-gl";
-import type { Map } from "mapbox-gl";
-import { useEffect, useRef, useState } from "react";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { Feature, FeatureCollection } from "geojson";
+import Map, { Source, Layer, type FillLayer, MapEvent } from "react-map-gl";
+import { Feature } from "geojson";
 import centroid from "@turf/centroid";
 
+const layerStyle: FillLayer = {
+  id: 'parcel-boundary',
+  type: 'fill',
+  paint: {
+    'fill-color': 'blue',
+    'fill-opacity': 0.5
+  }
+}
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
+const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
-export default function ParcelMap({ feature }: { feature: Feature }) {
-  const map = useRef<Map | null>(null);
-  const mapContainer = useRef(null);
-  const [lng, setLng] = useState(-0.25);
-  const [lat, setLat] = useState(52.57);
-  const [zoom, setZoom] = useState(10);
-
-  useEffect(() => {
-    if (map.current) return;
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current!,
-      style: "mapbox://styles/mapbox/satellite-streets-v12",
-      center: [lng, lat],
-      zoom: zoom
-    });
-
-    map.current.on("load", async () => {
-      map.current!.addSource("parcel", {
-        type: "geojson",
-        data: feature
-      });
-
-      map.current!.addLayer({
-        id: "parcel-boundary",
-        type: "fill",
-        source: "parcel",
-        paint: {
-          "fill-color": "blue",
-          "fill-opacity": 0.5
-        }
-      });
-
-      const target = {
-        center: centroid(feature).geometry.coordinates as [number, number],
-        zoom: 14,
-        pitch: 0,
-        bearing: 0
-      }
-      map.current!.flyTo({
-        ...target,
-        duration: 2000,
-        essential: true
-      });
-      // map.current!.fitBounds([[feature.bbox![0], feature.bbox![1]], [feature.bbox![2], feature.bbox![3]]]);
-    });
-  }, []); 
-
+export default function ParcelMap2({ feature }: { feature: Feature }) {
+  const center = centroid(feature).geometry.coordinates as [number, number];
+  const flyOnLoad = (event: MapEvent) => {
+    const map = event.target;
+    map.flyTo( {
+      center: center,
+      zoom: 15,
+      essential: true
+    })
+  }
   return (
-    <div>
-      <div ref={mapContainer} style={{ height: "400px" }} />
-    </div>
-  );
+    <Map
+      mapboxAccessToken={MAPBOX_TOKEN}
+      initialViewState={{
+        longitude: -0.25,
+        latitude: 52.57,
+        zoom: 10
+      }}
+      style={{width: '100%', height: '400px'}}
+      mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
+      onLoad={flyOnLoad}
+    >
+      <Source id="my-data" type="geojson" data={feature}>
+        <Layer {...layerStyle} />
+      </Source>
+    </Map> 
+  )
 }
