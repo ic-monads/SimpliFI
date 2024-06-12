5a19703d7e81d7d27edbff6c96e493cb0f2b69a4
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-12 17:42:43 +0100

    fix: :lipstick: Badge truncation

--- src/app/components/ActionBadges.tsx
+++ src/app/components/ActionBadges.tsx
@@ -2,10 +2,10 @@ import { Action } from "@prisma/client";
 
 export default function ActionBadges({ actions } : { actions: Action[] }) {
   return (
-    <div className="flex overflow-hidden gap-y-2">
+    <div className="flex flex-wrap gap-y-2">
       {actions.map((action) => 
-        <div key={action.code} className="badge badge-ghost text-xs mr-2 mt-2 max-w-[33%] overflow-hidden text-ellipsis whitespace-nowrap">
-          <span className="inline-block align-top truncate">{action.name} ({action.code})</span>
+        <div key={action.code} className="badge badge-ghost text-xs mr-2 truncate">
+          <span className="truncate">{action.name} ({action.code})</span>
         </div>
       )}
     </div>
