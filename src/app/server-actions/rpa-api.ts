4cd3b480ea9036f3b7958ae7ec27ea477b119d90
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-12 17:21:22 +0100

    fix: Use CRS:84 coordinate system

--- src/app/server-actions/rpa-api.ts
+++ src/app/server-actions/rpa-api.ts
@@ -2,7 +2,7 @@
 
 
 export async function fetchLandParcels(sbi: string) {
-    const url = `https://environment.data.gov.uk/data-services/RPA/LandParcels/wfs?version=2.0.0&request=GetFeature&typeNames=RPA:LandParcels&cql_filter=SBI=${sbi}&outputFormat=application/json`
+    const url = `https://environment.data.gov.uk/data-services/RPA/LandParcels/wfs?version=2.0.0&request=GetFeature&typeNames=RPA:LandParcels&cql_filter=SBI=${sbi}&srsname=CRS:84&outputFormat=application/json`
 
     const response = await fetch(url);
     const data = await response.json();
