efb944b2ba47bb31d8d9484144db36dd0426b2d0
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-12 11:11:32 +0100

    feat: Redirect from route path to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import Link from "next/link"
+import { redirect } from 'next/navigation'
 
 export default function Page() {
-
-  return (
-    <div className="flex flex-col h-screen">
-      <h1 className='text-center font-semibold mt-52 mb-12 text-2xl'><span className="text-green-600">S</span>impli<span className="text-green-600">FI</span></h1>
-      <div className="w-full flex justify-center items-center gap-6">
-        <Link href={{ pathname: "/login" }}>
-          <button className="btn btn-primary">Login</button>
-        </Link>
-        <Link href={{ pathname: "/signup" }}>
-          <button className="btn btn-primary">Sign Up</button>
-        </Link>
-      </div>
-
-    </div>
-
-  )
+  redirect("/login");
 }
