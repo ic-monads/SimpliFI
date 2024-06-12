93299dd32d7a3b4b6bea10b54ccf21202a8123a7
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-12 11:12:20 +0100

    refactor(Login): :recycle: Remove unnecessary HTML elements

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -6,7 +6,8 @@ export async function LoginForm() {
 
   return (
     <div className="w-full flex justify-center">
-      <div className="mx-auto mt-10">
+      <div className="mx-auto my-10">
+        <h1 className="text-2xl font-semibold text-center">Login</h1>
         {/* {!response!.success && <p className="text-red-500 text-sm mb-5">{response!.message}</p>} */}
         <form className="max-w-sm" action={attemptLogin}>
           <div className="label">
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,5 @@ import { LoginForm } from "../components/LoginForm";
 
 
 export default function Page() {
-  return (
-    <div className="w-full p-x">
-      <div className="flex flex-col py-52 w-full items-center justify-between">
-        <h1 className="text-2xl font-semibold">Login</h1>
-        <LoginForm />
-      </div>
-    </div>
-  )
+  return (<LoginForm />);
 }
