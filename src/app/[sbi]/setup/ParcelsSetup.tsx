bc1caf168f314fad6ac69055eb1c30d6907779e9
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2024-06-12 17:20:53 +0100

    feat(Setup): :sparkles: Save feature information when creating parcels on setup

--- src/app/[sbi]/setup/ParcelsSetup.tsx
+++ src/app/[sbi]/setup/ParcelsSetup.tsx
@@ -3,12 +3,15 @@
 import React from "react";
 import ParcelForm from "./ParcelForm";
 import { createFarmParcels } from "@/app/server-actions/farm";
+import { ParcelFeature } from "@/app/lib/types";
 
-export default function ParcelsSetup({ sbi, parcelIds }: { sbi: string, parcelIds: string[] }) {
-  const [parcels, setParcels] = React.useState<{ id: string, name: string }[]>(parcelIds.map((id) => ({ id, name: "" })));
+export default function ParcelsSetup({ sbi, features }: { sbi: string, features: ParcelFeature[] }) {
+  const [parcels, setParcels] = React.useState<{ id: string, name: string, feature: ParcelFeature }[]>(
+    features.map((feature) => ({ id: feature.properties.SHEET_ID + feature.properties.PARCEL_ID, name: "", feature }))
+  );
   const [parcelIndex, setParcelIndex] = React.useState(0);
 
-  const updateParcel = (parcel: { id: string, name: string }) => {
+  const updateParcel = (parcel: { id: string, name: string, feature: ParcelFeature }) => {
     const updatedParcels = parcels;
     updatedParcels[parcelIndex] = parcel;
     setParcels(updatedParcels);
@@ -28,6 +31,7 @@ export default function ParcelsSetup({ sbi, parcelIds }: { sbi: string, parcelId
         <h1 className="text-2xl font-semibold">Setup your parcels</h1>
         <ParcelForm parcel={parcels[parcelIndex]} updateParcel={updateParcel} nextParcel={nextParcel} />
       </div>
+
     </div>
   );
 }
