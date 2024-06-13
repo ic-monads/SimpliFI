f47b14e60dcfba1df02c3ea75a1aae44e547f7cb
Daniel Wilcox <danielgwilcox@gmail.com>
2024-06-13 18:54:25 +0100

    perf(PDF): Use saved data instead of requesting API

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { upload } from "@vercel/blob/client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Submit from "../components/Submit";
 import { parseAgreement, formatResult, PdfData } from "../server-actions/pdf";
 
@@ -9,26 +9,27 @@ function PDFForm() {
 
   // const status = useFormStatus();
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [parcelNumsAndCodes, setParcelNumsAndCodes] = useState<{ parcelNumber: string; code: string }[]>([]);
 
   const handleSubmit = async (formData: FormData) => {
-    // let fileUrl = "https://9c0ncerxleyeoepb.public.blob.vercel-storage.com/sfi-ZWlXpxPsAepGWSGUYZkLNhHDWaMt50.pdf";
-    let fileUrl = "";
-    if (fileInputRef.current?.files && fileInputRef.current.files.length > 0) {
-      console.log("Attempting file upload");
-      const file = fileInputRef.current.files[0];
-      const blob = await upload(file.name, file, {
-        access: "public",
-        handleUploadUrl: "/api/agreement/handle-upload",
-      });
+    let fileUrl = "https://9c0ncerxleyeoepb.public.blob.vercel-storage.com/sfi-ZWlXpxPsAepGWSGUYZkLNhHDWaMt50.pdf";
+    // let fileUrl = "";
+    // if (fileInputRef.current?.files && fileInputRef.current.files.length > 0) {
+    //   console.log("Attempting file upload");
+    //   const file = fileInputRef.current.files[0];
+    //   const blob = await upload(file.name, file, {
+    //     access: "public",
+    //     handleUploadUrl: "/api/agreement/handle-upload",
+    //   });
 
-      console.log("File uploaded successfully.");
-      fileUrl = blob.url;
-    }
+    //   console.log("File uploaded successfully.");
+    //   fileUrl = blob.url;
+    // }
 
     const parsedString = await parseAgreement(fileUrl);
     const dataJson = JSON.parse(parsedString) as PdfData;
     const formattedData = await formatResult(dataJson);
-    console.log(formattedData);
+    setParcelNumsAndCodes(formattedData);
 
   }
 
@@ -42,6 +43,27 @@ function PDFForm() {
         <input className="file-input file-input-bordered w-full" type="file" ref={fileInputRef} id="file" required />
         <Submit text="Submit Evidence" />
       </form >
+      {parcelNumsAndCodes.length > 0 && (<>
+        <h2 className="font-semibold text-2xl mt-5">Results</h2>
+        <div className="w-full sm:w-3/4 xl:w-1/2">
+          <table className="table-auto w-full">
+            <thead>
+              <tr>
+                <th>Parcel Number</th>
+                <th>Code</th>
+              </tr>
+            </thead>
+            <tbody>
+              {parcelNumsAndCodes.map((parcelNumAndCode, index) => (
+                <tr key={index}>
+                  <td>{parcelNumAndCode.parcelNumber}</td>
+                  <td>{parcelNumAndCode.code}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>)}
     </div >
   )
 }
